e42d3d0b27d87176d5da2e84237beae212f69731
MaidenQuinn <MaidenQuinn@users.noreply.github.com>
2019-10-17 15:33:42 +0200

    mise a jour de l'app et installation des modules nécessaire

---
+++ src/app/blog/article/article-similar/article-similar-item/article-similar-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-similar-item',
+  templateUrl: './article-similar-item.component.html',
+  styleUrls: ['./article-similar-item.component.scss']
+})
+export class ArticleSimilarItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
